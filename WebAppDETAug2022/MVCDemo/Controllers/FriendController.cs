ee9d89d96ccb27856577c47125e688c8d3d66020
Etg1 <Etg1@H9B2T72-SHEL>
2022-09-08 18:43:32 +0600

    add

---
+++ WebAppDETAug2022/MVCDemo/Controllers/FriendController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class FriendController : Controller
+    {
+        public List<Friend> friend { get; set; }
+
+    }
+}
